46fe1b4f3438b3ca96e063a2e08c5ac321c8f57f
Louie Birkett <birkettlouie@outlook.com>
2023-12-03 13:05:40 +0000

    Fully working buttons
    MainActivity.java - added Button objects
    activity_main.xml - changed drawable button and now button is responsive and changes color accordingly

--- app/src/main/java/com/example/copeapp/MainActivity.java
+++ app/src/main/java/com/example/copeapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.copeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pillButton = findViewById(R.id.button_pill);
+        Button horseButton = findViewById(R.id.button_horse);
+        Button smokeButton = findViewById(R.id.button_smoke);
+        Button sunButton = findViewById(R.id.button_sun);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+
+
+//        BUTTON ON CLICK LISTENERS
+        pillButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        horseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        smokeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        horseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sunButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
